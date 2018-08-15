c5438a21cad81d916ffdac912dda82440d76107f
kehuan <semapwang@gmail.com>
2018-08-15 11:51:38 -0700

    Use WeakReference of the context in all AsyncTasks

--- app/src/main/java/ca/aequilibrium/bbweather/managers/BookmarkedLocationManager.java
+++ app/src/main/java/ca/aequilibrium/bbweather/managers/BookmarkedLocationManager.java
@@ -16,13 +16,35 @@ import ca.aequilibrium.bbweather.utils.ResultCallback;
  */
 public interface BookmarkedLocationManager {
 
+    /**
+     * Add a BookmarkedCity by coordinator. The function will lookup the city name from the coordinator.
+     * @param coord the lat / lng of the bookmark.
+     * @param resultCallback The callback of the result. If successful, the callback will contain the
+     *                       newly created BookmarkedCity. If failed, the callback will contain the exception.
+     */
     void addBookmarkedCityByLocation(Coord coord, ResultCallback<BookmarkedCity> resultCallback);
 
-    // remove a bookmarked city
+    /**
+     * Remove a bookmarkedCities.
+     * @param resultCallback The result of this method, if successful, the result in the resultCallback
+     *                       is true. If failed, the result will have the exception.
+     */
     void remove(BookmarkedCity bookmarkedCity, ResultCallback<Boolean> resultCallback);
 
+    /**
+     * Remove all the bookmarkedCities.
+     * @param resultCallback The result of this method, if successful, the result has the number
+     *                       of BookmarkedCity that has been removed. If failed, the result will
+     *                       have the exception.
+     */
     void removeAll(ResultCallback<Integer> resultCallback);
 
     // The observable of the bookmarkedCities, this is the majoy "output" of the this manager
+
+    /**
+     * The LiveData which keeps the list of the BookmarkedCity. Whenever a list is modified,
+     *  the liveDate will emit a new element to notify the observers.
+     * @return
+     */
     LiveData<List<BookmarkedCity>> getBookmarkedCities();
 }
